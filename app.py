f971f7cda5a5f2d5bd8238b54416051a64d490de
Akshat Chauhan <akshat.chauhan@nexorityinfotech.com>
2025-04-23 20:29:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -77,13 +77,13 @@ summarization_tokenizer, summarization_model = download_summarization_model_from
 # abstract_tokenizer, abstract_model = load_abstract_model()
 
 import gdown
-# https://drive.google.com/drive/folders/14bj5dJ6mmbD9o-ATLzfNVScw1nVap0x7?usp=sharing
+# https://drive.google.com/drive/folders/1ft84VHVuVT7LZNzar442JOFqCyf4kLjT?usp=sharing
 @st.cache_resource
 def download_abstract_model_from_drive():
     model_path = "model"
     if not os.path.exists(model_path):
         gdown.download_folder(
-            "https://drive.google.com/drive/folders/14bj5dJ6mmbD9o-ATLzfNVScw1nVap0x7?usp=sharing", quiet=False, use_cookies=False
+            "https://drive.google.com/drive/folders/1ft84VHVuVT7LZNzar442JOFqCyf4kLjT?usp=sharing", quiet=False, use_cookies=False
         )
     tokenizer = LEDTokenizerFast.from_pretrained(model_path)
     model = LEDForConditionalGeneration.from_pretrained(model_path)
